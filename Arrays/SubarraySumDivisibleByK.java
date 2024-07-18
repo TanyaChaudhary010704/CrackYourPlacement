5bd5eab7a77d6203aa74b1b8616c8079a1389c24
TanyaChaudhary010704 <punch010704@gmail.com>
2024-07-18 13:47:24 +0530

    Update SubarraySumDivisibleByK.java

--- Arrays/SubarraySumDivisibleByK.java
+++ Arrays/SubarraySumDivisibleByK.java
@@ -1,15 +1,21 @@
 package Arrays;
 
 class Solution {
-  public int subarraysDivByK(int[] nums, int K) {
-      int[] count = new int[K];
-      count[0] = 1;
-      int prefix = 0, res = 0;
-      for (int num : nums) {
-          prefix = (prefix + num % K + K) % K;
-          res += count[prefix]++;
-      }
-      return res;
+  public int subarraysDivByK(int[] nums, int k) {
+    int count[] = new int[k];
+    count[0]=1;
+    int cummulativeSum =0;
+    int answer=0;
+    for(int num : nums){
+        cummulativeSum+= num;
+        int remainder = cummulativeSum%k;
+        if(remainder<0){
+            remainder +=k;
+        }
+        answer +=count[remainder];
+        count[remainder]++;
+    }
+    return answer;
   }
 }
 public class SubarraySumDivisibleByK {
