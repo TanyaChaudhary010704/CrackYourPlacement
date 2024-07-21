c0b9a99d48feb4ff0b394b2c66f284cbb06399d5
TanyaChaudhary010704 <punch010704@gmail.com>
2024-07-21 20:30:53 +0530

    Create ThreeSum.java

---
+++ Arrays/ThreeSum.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+import java.util.*;
+
+public class ThreeSum {
+  public static void main(String[] args) {
+    int nums[] = { 1,-1,-1,0,2};
+    Solution s = new Solution();
+    System.out.println(s.threeSum(nums));
+
+  }
+}
+class Solution {
+  public List<List<Integer>> threeSum(int[] nums) {
+    if (nums == null || nums.length < 3) {
+      return new ArrayList<>();
+    }
+    Arrays.sort(nums);
+    Set<List<Integer>> result = new HashSet<>();
+    for (int i = 0; i < nums.length - 2; i++) {
+      int left = i + 1;
+      int right = nums.length - 1;
+      while (left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
+        if (sum == 0) {
+          result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+          left++;
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+    return new ArrayList<>(result);
+  }
+}
