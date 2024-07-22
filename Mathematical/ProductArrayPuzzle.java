4979d725bc2b4961babeff815e9342bd20652b14
TanyaChaudhary010704 <punch010704@gmail.com>
2024-07-22 23:40:48 +0530

    Create ProductArrayPuzzle.java

---
+++ Mathematical/ProductArrayPuzzle.java
@@ -0,0 +1,55 @@
+package Mathematical;
+public class ProductArrayPuzzle {
+  public static void main(String[] args) {
+    int nums[] = {10,3,5,6,2};
+    System.out.println(productExceptSelf(nums, 5));
+  }
+
+	public static long[] productExceptSelf(int nums[], int n) 
+	{ 
+    long total=1;
+    long zeroCount=0;
+    int zeroPosition=-1;
+    long res[]= new long[nums.length];
+    for(int i=0;i<nums.length;i++)
+    {
+        if(nums[i]!= 0)
+        {
+            total*=(long)nums[i];
+        }
+        else{
+            zeroCount++;
+            zeroPosition=i;
+        }
+    }
+    if(zeroCount>1)
+    {
+        for(int i=0;i<res.length;i++)
+        {
+            res[i]=0;
+        }
+        return res;
+    }
+    for(int i=0;i<res.length;i++)
+    {
+        if(zeroCount==1)
+        {
+            if(zeroPosition==i)
+            {
+                res[i]=total;
+            }
+            else
+            {
+                res[i]=0;
+            }
+        }
+        else
+        {
+            res[i]=total/(long)nums[i];
+        
+        }
+    }
+    return res;
+	} 
+} 
+
