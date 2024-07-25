1595ba2fc6bc94b7943bacd7d55c1f3b1fc7f836
TanyaChaudhary010704 <punch010704@gmail.com>
2024-07-25 16:10:03 +0530

    removed errors

--- String/LongestCommonPrefix.java
+++ String/LongestCommonPrefix.java
@@ -4,23 +4,20 @@ import java.util.Arrays;
 public class LongestCommonPrefix {
   public static void main(String[] args) {
     String[] strs = {"flo"};
-    Solution s = new Solution();
-    System.out.println(s.longestCommonPrefix(strs));
+    System.out.println(longestCommonPrefix(strs));
   } 
-}
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-     
-        StringBuilder common = new StringBuilder();
-        Arrays.sort(strs);
-        char[] first = strs[0].toCharArray();
-        char[] last = strs[strs.length-1].toCharArray();
-        for(int i=0;i<first.length;i++){
-            if(first[i]!=last[i]){
-                break;
-            }
-            common.append(first[i]);
-        }
-        return common.toString();   
-    }
+  public static String longestCommonPrefix(String[] strs) {
+    
+      StringBuilder common = new StringBuilder();
+      Arrays.sort(strs);
+      char[] first = strs[0].toCharArray();
+      char[] last = strs[strs.length-1].toCharArray();
+      for(int i=0;i<first.length;i++){
+          if(first[i]!=last[i]){
+              break;
+          }
+          common.append(first[i]);
+      }
+      return common.toString();   
+  }
 }
--- String/ValidParenthesis.java
+++ String/ValidParenthesis.java
@@ -1,34 +1,27 @@
 package String;
 import java.util.Stack;
-class Solution {
-  public boolean isValid(String s) {
-      Stack<Character> stack = new Stack<>();
-      for(char ch : s.toCharArray()){
-          if(ch=='('||ch=='{'|| ch=='['){
-              stack.push(ch);
-          }
-          else if(stack.isEmpty()){
-              return false;
-          }
-          else if((ch==')'&&stack.peek()=='(')||
-                  (ch=='}'&&stack.peek()=='{')||
-                  (ch==']'&&stack.peek()=='[')){
-              stack.pop();
-          }
-          else{
-              stack.push(ch);
-          }
+public class ValidParenthesis{
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else if ((ch == ')' && stack.peek() == '(') ||
+                    (ch == '}' && stack.peek() == '{') ||
+                    (ch == ']' && stack.peek() == '[')) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isValid("([])"));
       }
-      if(stack.isEmpty()){
-          return true;
-      }
-      return false;
-  }
-}
-
-public class ValidParenthesis {
-  public static void main(String[] args) {
-    Solution s = new Solution();
-    System.out.println(s.isValid("([])"));
-  }
 }
