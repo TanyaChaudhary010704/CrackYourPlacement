a3b89f0f93439f43021a6620395a6f4d090ae95b
TanyaChaudhary010704 <punch010704@gmail.com>
2024-07-28 18:53:08 +0530

    Create MiddleOfLL.java

---
+++ LinkedList/MiddleOfLL.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+public class MiddleOfLL {
+  public ListNode middleNode(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
+}
